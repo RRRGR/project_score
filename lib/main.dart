282d5e6ea52367ba80758afe011a55d0d95f2c5c
GrGuruto <82594900+GrGuruto@user.noreply.github.com>
2023-02-13 15:47:57 +0900

    add conastant material white

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:project_score/model/constant.dart';
 import 'package:project_score/view/screen/home.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -16,35 +17,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  final MaterialColor materialWhite = const MaterialColor(
-    0xFFFFFFFF,
-    <int, Color>{
-      50: Color(0xFFFFFFFF),
-      100: Color(0xFFFFFFFF),
-      200: Color(0xFFFFFFFF),
-      300: Color(0xFFFFFFFF),
-      400: Color(0xFFFFFFFF),
-      500: Color(0xFFFFFFFF),
-      600: Color(0xFFFFFFFF),
-      700: Color(0xFFFFFFFF),
-      800: Color(0xFFFFFFFF),
-      900: Color(0xFFFFFFFF),
-    },
-  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const Home(),
       theme: ThemeData(primarySwatch: materialWhite),
-      // theme: ThemeData.light().copyWith(
-      //   appBarTheme: const AppBarTheme(
-      //     backgroundColor: Colors.transparent,
-      //     foregroundColor: Colors.black,
-      //     elevation: 0,
-      //     systemOverlayStyle: SystemUiOverlayStyle.dark,
-      //   ),
-      // ),
     );
   }
 }
